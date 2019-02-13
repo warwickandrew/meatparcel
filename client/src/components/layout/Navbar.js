c73315d3844e7bdaab01450d29811ab77745dd74
Andrew Warwick <andrew.warwick@learningpool.com>
2019-02-13 08:35:21 +0000

    pages layouts and routes added

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom';
       <div>
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
           <div className="container">
-            <Link className="navbar-brand" to="/">DevConnector</Link>
+            <Link className="navbar-brand" to="/">Meat2You</Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
               <span className="navbar-toggler-icon"></span>
             </button>
@@ -15,7 +15,7 @@ import { Link } from 'react-router-dom';
             <div className="collapse navbar-collapse" id="mobile-nav">
               <ul className="navbar-nav mr-auto">
                 <li className="nav-item">
-                  <Link className="nav-link" to="/profiles"> Developers
+                  <Link className="nav-link" to="/profiles"> Butcher Shops
                   </Link>
                 </li>
               </ul>
